6cb1014d0c18546e81b0f014dc92f6d256093b4d
Dmitry Pluzhnikov <dmitry.pluzhnikov@cyoda.com>
2023-01-10 17:16:57 +0300

    - rollback adding date field to split

--- presto-cyoda/src/main/java/com/cyoda/presto/CyodaSplit.java
+++ presto-cyoda/src/main/java/com/cyoda/presto/CyodaSplit.java
@@ -43,43 +43,32 @@ public class CyodaSplit implements ConnectorSplit {
 
     private final String groupJsonBase64;
 
-    private final Date createDate;
 
     private final int page;
 
     private final int size;
 
-    private transient long startExecution = -1;
-    private transient long endExecution = -1;
 
     @JsonCreator
-    public CyodaSplit(List<HostAddress> addresses, String tableName, String reportConfigId, String reportId, UUID groupingVersion, String groupJsonBase64, Date createDate, int page, int size) {
+    public CyodaSplit(List<HostAddress> addresses, String tableName, String reportConfigId, String reportId, UUID groupingVersion, String groupJsonBase64, int page, int size) {
         this.addresses = addresses;
         this.tableName = tableName;
         this.reportConfigId = reportConfigId;
         this.reportId = reportId;
         this.groupingVersion = groupingVersion;
         this.groupJsonBase64 = groupJsonBase64;
-        this.createDate = createDate;
         this.page = page;
         this.size = size;
     }
 
     public CyodaSplit(String tableName, String reportConfigId, String reportId, UUID groupingVersion, String groupJsonBase64){
-        this(Collections.emptyList(), tableName, reportConfigId, reportId, groupingVersion, groupJsonBase64, new Date(), 0, Integer.MAX_VALUE-1);
+        this(Collections.emptyList(), tableName, reportConfigId, reportId, groupingVersion, groupJsonBase64, 0, Integer.MAX_VALUE-1);
     }
 
     public static CyodaSplit emptySplit(String tableName){
         return new CyodaSplit(tableName, null, null, null, null);
     }
 
-    public void startExecution(){
-        startExecution = System.currentTimeMillis() - createDate.getTime();
-    }
-
-    public void endExecution(){
-        endExecution = System.currentTimeMillis() - startExecution - createDate.getTime();
-    }
     @JsonProperty
     public String getTableName() {
         return tableName;
