74e715a166380dc99040da5c7199c26252adb040
Dmitry Pluzhnikov <dmitry.pluzhnikov@cyoda.com>
2024-11-12 18:08:01 +0300

    CP-3404 removed unnecessary type conversion for pushed down conditions

--- cyoda-connector/src/main/java/com/cyoda/connector/client/logic/converters/PrestoValueConverter.java
+++ cyoda-connector/src/main/java/com/cyoda/connector/client/logic/converters/PrestoValueConverter.java
@@ -47,7 +47,7 @@ public interface PrestoValueConverter<T> {
         throw new UnsupportedOperationException("Condition pushdown is not supported for " + getDataType());
     }
     default String toStringFromNative(Object nativeValue){
-        return fromPrestoNative(nativeValue).toString();
+        return nativeValue.toString();
     }
 
     default boolean areConsecutive(T a, T b){
